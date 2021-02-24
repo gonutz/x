aa4b9c96b83accf5b054a46fea4fdcb7cb8fde74
lars <lars.scheme@gmail.com>
2021-02-24 22:37:56 +0100

    Add module support

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gonutz/x
+
+go 1.11
+
+require github.com/gonutz/w32/v2 v2.0.1
--- main_windows.go
+++ main_windows.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/gonutz/w32"
+import "github.com/gonutz/w32/v2"
 
 func main() {
 	w32.SendMessage(w32.GetConsoleWindow(), w32.WM_CLOSE, 0, 0)
